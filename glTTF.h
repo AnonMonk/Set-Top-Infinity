b0b22e589b8023ebb7121758baf73ac5f9f02952
AnonMonk <monkxvjm@yahoo.com>
2026-08-10 15:37:26 +0200

    VipFPX Update

--- glTTF.h
+++ glTTF.h
@@ -7,7 +7,6 @@
 typedef struct
 {
     void *data;
-    unsigned int size;
 } aFont;
 
 #ifdef __cplusplus
@@ -17,6 +16,7 @@ extern "C" {
 void loadFont(const char *fname, int h, aFont *fnt);
 void closeFont(aFont *fnt);
 void printText(int32_t x, int32_t y, const char *s, uint32_t color, aFont fnt);
+void getTextSize(aFont fnt, const char *s, uint32_t *width, uint32_t *height);
 
 #ifdef __cplusplus
 }
